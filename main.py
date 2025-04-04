9adae1d314ac10690602eafe65e25a2e51f65816
Arthur-Firm <arthurfirminomartins@gmail.com>
2025-04-04 16:08:33 -0300

    Configurações iniciais

---
+++ main.py
@@ -0,0 +1,14 @@
+import google.generativeai as genai
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+import os
+
+login = os.environ.get("login")
+senha = os.environ.get("senha")
+api_key = os.environ.get("api_key")
+
