122b9713d938cb7eba88a30d2f42630421a731ec
Arthur-Firm <arthurfirminomartins@gmail.com>
2025-04-07 18:18:56 -0300

    Login no site finalizado

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
 
-login = os.environ.get("login")
-senha = os.environ.get("senha")
+
 api_key = os.environ.get("api_key")
 
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 navegador.get('https://www.linkedin.com/feed/')
+
+def realizar_login():
+    login = os.environ.get("login")
+    senha = os.environ.get("senha")
+    navegador.find_element(By.XPATH, '//input[@id="username"]').send_keys(login)
+    navegador.find_element(By.XPATH, '//input[@id="password"]').send_keys(senha)
+    navegador.find_element(By.XPATH, '//label[@for="rememberMeOptIn-checkbox"]').click()
+    navegador.find_element(By.XPATH, '//button[@type="submit"]').click()
+    input()
